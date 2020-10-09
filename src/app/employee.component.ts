3ac56c45c460d79e64808e3d8bcd40695147a403
SuyedDalvi <suyed786@gmail.com>
2020-10-09 12:36:05 +0530

    1 file modified, 1 added

--- src/app/employee.component.ts
+++ src/app/employee.component.ts
@@ -1,21 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import {  FormBuilder  } from '@angular/forms';
 
 @Component({
   selector: 'employee',
-  template: `<h1>Hello {{name}}!</h1>
-              <form [formGroup]="form">
-              <div formArrayName="names">
-                <div *ngFor='let emp of getNames().controls; let i = index' [formGroupName]="i">
-                  
-                  <input type="text" formControlName="userId">
-                 
-                </div>
-              </div>
-                
-              </form>
-              <br/>
-              {{form.value | json}}
+  template: `<h1>Hello {{name}}!</h1>              
               <div *ngFor= 'let emp of employees'>
                  {{emp.userId}} 
               </div>
@@ -25,26 +13,16 @@ import { AbstractControl, FormArray, FormBuilder, FormGroup } from '@angular/for
 export class EmployeeComponent  {
   @Input() name: string;
   @Input() employees: any[];
-  form: FormGroup;
+ 
 
   constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      names: this.fb.array([])
-    })
+    
   }
   ngOnInit() {
     console.log(this.employees);
-    this.employees.forEach((emp) => {
-        (this.form.get('names') as FormArray).push(this.fb.group({
-          userId: [emp.userId]
-        }));
-    });
+    
     
   }
-  getNames() : FormArray {
-    return (this.form.get('names') as FormArray);
-  }
-  display(e) {
-    console.log(e);
-  }
+ 
+  
 }
